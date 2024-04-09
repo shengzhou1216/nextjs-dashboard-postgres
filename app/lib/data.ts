47f9b3002a59c12cbcddc1523dd45fd2a6c35c45
shengzhou1216 <zhousheng1216@sina.com>
2024-04-09 19:01:29 +0800

    init

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -216,7 +216,7 @@ export async function fetchFilteredCustomers(query: string) {
     return customers;
   } catch (err) {
     console.error('Database Error:', err);
-    throw new Error('Failed to fetch customer table.');
+    throw new Error('Failed to fetch customers table.');
   }
 }
 
