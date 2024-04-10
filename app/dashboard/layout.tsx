c42aa9f3125a41b44512068df3926c2fd4744ffd
huozaimengli <shengzhouzzzzzz1216@gmail.com>
2024-04-10 17:40:52 +0800

    finish

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import SideNav from "@/app/ui/dashboard/sidenav";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Home',
+
+    }
+}
 
 export default  function Layout({children} : {children: React.ReactNode} ){
     return (
